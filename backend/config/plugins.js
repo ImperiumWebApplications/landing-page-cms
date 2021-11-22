2bd92f5301cbc869f266554e58df7e7ac4c90348
Konstantin <konstantin.muenster@mail.com>
2021-11-22 15:14:47 +0100

    add mono repo setup

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "aws-s3",
+    provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION"),
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
       params: {
-        Bucket: env("AWS_BUCKET_NAME"),
+        Bucket: env('AWS_BUCKET_NAME'),
       },
     },
   },
