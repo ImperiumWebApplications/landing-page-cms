5355223d6774e968d98791cd2d5f6d1fc6af63cd
Konstantin <konstantin.muenster@mail.com>
2021-11-22 09:34:09 +0100

    init repository

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a35bf31628409c421ec2fdfece1c010'),
+    },
+  },
+});
