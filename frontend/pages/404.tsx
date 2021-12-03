ffe41a0ec3f7505911e8f0eb565f0b4d65c81fb3
Konstantin <konstantin.muenster@mail.com>
2021-12-03 19:52:20 +0100

    add 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,7 +1,44 @@
 import type { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Styled404 = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  font-size: 1rem;
+  font-weight: 400;
+  letter-spacing: +0.25px;
+  line-height: 1.5rem;
+  color: #aaa69d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20% 0;
+  h1 {
+    font-size: 1.5rem;
+  }
+  a {
+    background-color: #aaa69d;
+    color: white;
+    padding: 0.15rem 0.25rem;
+    font-size: 0.9rem;
+    font-weight: bold;
+    text-decoration: none;
+    border-radius: 0.125rem;
+  }
+`;
 
 const NotFound: NextPage = () => {
-  return <h1>404</h1>;
+  return (
+    <Styled404>
+      <main>
+        <h1>404 – Diese Seite existiert leider nicht.</h1>
+        <p>
+          <a href="/">Hier</a> kehrst du zur Startseite zurück.
+        </p>
+      </main>
+    </Styled404>
+  );
 };
 
 export default NotFound;
