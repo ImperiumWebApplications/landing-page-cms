62b36b8a87ebf933c2080d3554bb3d7f4345ae61
Konstantin <konstantin.muenster@mail.com>
2021-12-03 10:00:18 +0100

    add old eslint config to backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,3 +1,28 @@
 module.exports = {
-  extends: ['@shared/eslint-config'],
+  parser: '@babel/eslint-parser',
+  extends: ['eslint:recommended', 'eslint-config-prettier'],
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    browser: false,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: false,
+    },
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  globals: {
+    strapi: true,
+  },
+  plugins: ['eslint-plugin-prettier'],
+  rules: {
+    'no-console': 0,
+    'prettier/prettier': 'error',
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+  },
 };
