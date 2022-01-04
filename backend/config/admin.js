997d321be15f321cdec0b36f543a4ba5f08bfcb8
Konstantin <konstantin.muenster@mail.com>
2022-01-04 13:33:01 +0100

    configure strapi admin settings

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
+  url: '/dashboard',
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'f34db2dd79cefd80214cd5cb8521a803'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '234db2dds3cef480214cd5cb8521a803'),
   },
 });
