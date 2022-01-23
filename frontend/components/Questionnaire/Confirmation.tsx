7385ace9f9fd94dea10d244f07be545ce6a073cc
Konstantin <konstantin.muenster@mail.com>
2022-01-23 14:31:43 +0100

    tweak design on confirmation page

--- frontend/components/Questionnaire/Confirmation.tsx
+++ frontend/components/Questionnaire/Confirmation.tsx
@@ -2,24 +2,23 @@ import React from 'react';
 import styled, { useTheme } from 'styled-components';
 import { CheckmarkCircle } from '@styled-icons/fluentui-system-regular';
 
+import { StyledStepTitle } from './StepTitle';
+
 const StyledConfirmation = styled.div`
   width: 100%;
   max-width: 40rem;
   margin: 0 auto;
 
   .icon {
-    margin: 0 auto;
+    margin: -1rem auto 2rem auto;
     width: 7.5rem;
     height: 7.5rem;
-  }
-
-  .title {
-    margin: 2rem 0;
   }
 
   .subtitle {
     margin: 1.5rem 0 0.5rem 0;
     font-size: 1.25rem;
+    line-height: 1.5rem;
 
     & + p {
       font-size: 1.125rem;
@@ -43,8 +42,10 @@ export const Confirmation: React.FunctionComponent<{ phone?: string }> = ({
           opacity={0.5}
         />
       </div>
-      <div className="content">
-        <h1 className="title">Ihre Anfrage wurde erfolgreich übermittelt.</h1>
+      <div>
+        <StyledStepTitle>
+          Ihre Anfrage wurde erfolgreich übermittelt.
+        </StyledStepTitle>
         <p>
           Zeitnah erhalten Sie von uns einen Anruf, damit wir Ihre Angaben
           gemeinsam validieren können. Kurz darauf werden Sie mindestens ein
