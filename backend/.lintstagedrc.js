cd013066a341c24e73759b5f48bf50185a0d2b2d
Konstantin <konstantin.muenster@mail.com>
2022-04-04 17:54:18 +0200

    remove ebextensions in dev environment

--- backend/.lintstagedrc.js
+++ backend/.lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.js': ['eslint'],
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
+  '*.+(js|jsx|json|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
   ],
 };
