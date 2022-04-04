a565c661f4f4ec8bd3f32ff53ec56126654394e5
Konstantin <konstantin.muenster@mail.com>
2022-04-04 16:16:15 +0200

    remove console log

--- frontend/utils/isFormDataComplete.ts
+++ frontend/utils/isFormDataComplete.ts
@@ -5,7 +5,6 @@ import {
 import type { QuestionnaireContextState } from '../context/Questionnaire/state';
 
 export const isFormDataComplete = (state: QuestionnaireContextState) => {
-  console.log(state);
   return !Object.entries(state.contact).some(([key, field]) => {
     if (typeof field.value === 'boolean') return field.value === false;
     if (hiddenFieldsOnContactForm.indexOf(key as ContactFields) !== -1)
