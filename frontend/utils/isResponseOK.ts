b584e8bd48715677421006f97363a3d25afb2529
Konstantin <konstantin.muenster@mail.com>
2022-04-12 10:44:57 +0200

    add tests for isResponseOK

--- frontend/utils/isResponseOK.ts
+++ frontend/utils/isResponseOK.ts
@@ -12,7 +12,7 @@ export const isContentObjectOK = <T>(res: {
 export const isContentObjectListOK = <T>(res: {
   status: number;
   data: BackendAPIResponse<DataObject<T>[]>;
-}) => res.status >= 200 && res.status < 300 && res.data.data.length;
+}) => res.status >= 200 && res.status < 300 && res.data.data?.length;
 
 export const isPipedriveDataOK = (res: {
   status: number;
