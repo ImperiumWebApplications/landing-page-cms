ff8aed136fa75546f78370cfe0248609a5a1e61f
Konstantin <konstantin.muenster@mail.com>
2022-04-12 14:57:58 +0200

    fix styling for safari

--- frontend/config/theme.config.ts
+++ frontend/config/theme.config.ts
@@ -134,4 +134,10 @@ export const GlobalStyle = createGlobalStyle<{
       transition: all 550ms cubic-bezier(0.19, 1, 0.22, 1);
     }
   }
+
+  input:focus {
+    outline-offset: -2px;
+    outline-width: 1px;
+    outline-color: ${({ theme }) => theme.colors.secondary};
+  }
 `;
