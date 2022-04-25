e5c5b46055acf62066ac63e323dec0184a2aeb84
Konstantin <konstantin.muenster@mail.com>
2022-04-25 09:21:01 +0200

    improve healthcheck test

--- frontend/pages/api/healthcheck.ts
+++ frontend/pages/api/healthcheck.ts
@@ -1,9 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from '@sentry/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(
+import { StrapiAPI } from '../../interface/backend';
+import { getErrorMessage } from '../../lib/api/error';
+
+export const handler = async (
   _: NextApiRequest,
   res: NextApiResponse<{ status: string }>,
-) {
-  res.status(200).json({ status: 'ok' });
-}
+) => {
+  try {
+    const backend = await StrapiAPI.getStaticLandingPageContent();
+    if (!backend) throw new Error('Backend not available');
+    res.status(200).json({ status: 'ok' });
+  } catch (e) {
+    res.status(500).json({ status: getErrorMessage(e) });
+  }
+};
+
+export default withSentry(handler);
