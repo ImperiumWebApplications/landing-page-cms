4339649ed342232bca2b8c328bdf8c44c7c74968
Konstantin <konstantin.muenster@mail.com>
2022-05-30 08:47:58 +0200

    update ssl certificates script

--- aws/generateCertificatesRenewal.mjs
+++ aws/generateCertificatesRenewal.mjs
@@ -1,8 +1,8 @@
 const generateCertificatesRenewal = () => {
   const MAIN_DOMAIN = "craftsman24.ch";
-  const CUSTOM_DOMAINS = [];
-  const REQUESTED_CUSTOM_DOMAINS = CUSTOM_DOMAINS.map((d) => `${d} *.${d}`);
-  return `aws acm request-certificate --domain-name ${MAIN_DOMAIN} --validation-method DNS --subject-alternative-names *.${MAIN_DOMAIN} ${REQUESTED_CUSTOM_DOMAINS.join(
+  const CUSTOM_DOMAINS = ["craftsman24.de"];
+  const REQUESTED_CUSTOM_DOMAINS = CUSTOM_DOMAINS.map((d) => `${d} '*.${d}'`);
+  return `aws acm request-certificate --profile leadquelle --tags Key='landing-page-cms',Value=frontend --domain-name ${MAIN_DOMAIN} --validation-method DNS --subject-alternative-names '*.${MAIN_DOMAIN}' ${REQUESTED_CUSTOM_DOMAINS.join(
     " "
   )}`;
 };
