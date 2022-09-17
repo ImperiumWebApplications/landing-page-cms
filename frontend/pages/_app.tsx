4bead2eab27b95d8946653dc9f1c495189e8ab32
Konstantin <konstantin.muenster@mail.com>
2022-09-17 15:07:31 +0200

    disable e2e mocking temporarily

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,15 +7,15 @@ import 'swiper/css/pagination';
 import '@fontsource/overpass';
 import '@fontsource/overpass/700.css';
 
-import { isTestEnvironment } from '../utils/isTestEnvironment';
+// import { isTestEnvironment } from '../utils/isTestEnvironment';
 
-if (isTestEnvironment()) {
-  (async () => {
-    const { backendAPIMockHandlers } = await import('../mocks/backend-api');
-    const { startAPIMockServer } = await import('../mocks/utils/mock-rest-api');
-    await startAPIMockServer(backendAPIMockHandlers);
-  })();
-}
+// if (isTestEnvironment()) {
+//   (async () => {
+//     const { backendAPIMockHandlers } = await import('../mocks/backend-api');
+//     const { startAPIMockServer } = await import('../mocks/utils/mock-rest-api');
+//     await startAPIMockServer(backendAPIMockHandlers);
+//   })();
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
