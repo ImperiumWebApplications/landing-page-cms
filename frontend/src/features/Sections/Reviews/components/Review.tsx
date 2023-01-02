d25ea95e79171fc7a39fb1d860bd1784e1b0d8d0
Konstantin <konstantin.muenster@mail.com>
2023-01-02 13:35:19 +0100

    improve reviews

--- frontend/src/features/Sections/Reviews/components/Review.tsx
+++ frontend/src/features/Sections/Reviews/components/Review.tsx
@@ -13,7 +13,7 @@ export const Review: React.FC<ReviewProps> = ({ content }) => {
   const [isExpanded, setIsExpanded] = React.useState(false);
 
   return (
-    <div className="relative mx-2 mt-14 mb-8 rounded-md bg-[white] px-12 pt-20 pb-8 shadow-md sm:mx-8 sm:px-8 md:px-20 md:pt-24 md:pb-16">
+    <div className="relative mx-2 mt-14 mb-8 rounded-md bg-[white] px-6 pt-20 pb-8 shadow-md sm:mx-8 md:px-20 md:pt-24 md:pb-16">
       <div className="absolute -top-11 left-[calc(50%-50px)] z-10 h-[100px] w-[100px] before:absolute before:-left-[5px] before:-top-1 before:-z-[1] before:h-[55px] before:w-[110px] before:rounded-tl-md before:rounded-tr-md before:bg-primary before:content-[''] after:absolute after:-left-[5px] after:bottom-0 after:-z-[1] after:h-[55px] after:w-[110px] after:rounded-br-md after:rounded-bl-md after:bg-tertiary after:content-['']">
         {content.avatar?.data?.attributes ? (
           <Image
@@ -26,7 +26,7 @@ export const Review: React.FC<ReviewProps> = ({ content }) => {
         ) : null}
       </div>
       {content.description && (
-        <p className="mb-8 leading-relaxed">
+        <p className="mb-8 text-sm leading-relaxed sm:text-base">
           {isTabletBreakpoint || isExpanded
             ? content.description
             : `${content.description?.substring(0, 300)}...`}
