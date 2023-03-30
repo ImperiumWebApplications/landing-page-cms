ca2ac39f8baf780f402ade6ff93b5caf188eae7a
Konstantin <konstantin.muenster@mail.com>
2023-03-30 19:48:11 +0200

    update ssl certificate domains

--- scripts/generateCertificatesRenewal.mjs
+++ scripts/generateCertificatesRenewal.mjs
@@ -1,6 +1,6 @@
 const generateCertificatesRenewal = () => {
-  const MAIN_DOMAIN = "craftsman24.ch";
-  const CUSTOM_DOMAINS = ["craftsman24.de"];
+  const MAIN_DOMAIN = "leadquelle.net";
+  const CUSTOM_DOMAINS = ["craftsman24.ch", "craftsman24.de"];
   const REQUESTED_CUSTOM_DOMAINS = CUSTOM_DOMAINS.map((d) => `${d} '*.${d}'`);
   return `aws acm request-certificate --profile leadquelle --tags Key='landing-page-cms',Value=frontend --domain-name ${MAIN_DOMAIN} --validation-method DNS --subject-alternative-names '*.${MAIN_DOMAIN}' ${REQUESTED_CUSTOM_DOMAINS.join(
     " "
