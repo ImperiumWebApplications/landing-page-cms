c89ab38a17a0f944b6a02fb4678c6215206daeb1
Konstantin <konstantin.muenster@mail.com>
2023-04-11 13:01:24 +0200

    fix overflowing step title

--- apps/frontend/src/features/Questionnaire/components/StepTitle.tsx
+++ apps/frontend/src/features/Questionnaire/components/StepTitle.tsx
@@ -4,7 +4,7 @@ export const StepTitle: React.FC<{ children?: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <h2 className="mb-5 text-center text-base md:mb-12 md:text-[22px]">
+    <h2 className="mb-5 max-w-3xl text-center text-base md:mb-12 md:text-[22px] md:leading-[32px]">
       {children}
     </h2>
   );
