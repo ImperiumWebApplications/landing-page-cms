8c7a01216881db8329c0849f484f2755618b2b36
Konstantin <konstantin.muenster@mail.com>
2023-05-09 14:17:01 +0200

    update custom domain list

--- scripts/generateCertificatesRenewal.mjs
+++ scripts/generateCertificatesRenewal.mjs
@@ -1,6 +1,11 @@
 const generateCertificatesRenewal = () => {
   const MAIN_DOMAIN = "leadquelle.net";
-  const CUSTOM_DOMAINS = ["craftsman24.ch", "craftsman24.de", "lq-pages.ch"];
+  const CUSTOM_DOMAINS = [
+    "craftsman24.ch",
+    "craftsman24.de",
+    "lq-pages.ch",
+    "autothai24.com",
+  ];
   const REQUESTED_CUSTOM_DOMAINS = CUSTOM_DOMAINS.map((d) => `${d} '*.${d}'`);
   return `aws acm request-certificate --profile leadquelle --tags Key='landing-page-cms',Value=frontend --domain-name ${MAIN_DOMAIN} --validation-method DNS --subject-alternative-names '*.${MAIN_DOMAIN}' ${REQUESTED_CUSTOM_DOMAINS.join(
     " "
