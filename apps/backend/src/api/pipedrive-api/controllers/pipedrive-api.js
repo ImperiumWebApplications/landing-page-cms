a879067be35cfc5340cfd627a2a52a9cb23bf045
Konstantin <konstantin.muenster@mail.com>
2023-05-29 14:40:00 +0200

    remove Pipedrive in backend

--- apps/backend/src/api/pipedrive-api/controllers/pipedrive-api.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  pipedrive-api controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::pipedrive-api.pipedrive-api');
--- apps/backend/src/api/pipedrive-api/services/pipedrive-api.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * pipedrive-api service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::pipedrive-api.pipedrive-api');
