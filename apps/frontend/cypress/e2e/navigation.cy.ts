43d9ecf9e29ce8a2b35cd3379d84c4ea140cc412
Aditya Singh <adityasingh220598@gmail.com>
2023-07-13 15:24:32 +0530

    Fix: cypress test updated

--- apps/frontend/cypress/e2e/navigation.cy.ts
+++ apps/frontend/cypress/e2e/navigation.cy.ts
@@ -18,7 +18,7 @@ describe('Navigation', () => {
 
   it('should navigate via footer', () => {
     cy.get('footer nav > a')
-      .last()
+      .eq(1)
       .click()
       .assertUrl('/datenschutz')
       .get('#privacy')
