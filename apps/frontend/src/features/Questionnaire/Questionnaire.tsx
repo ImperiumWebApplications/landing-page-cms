c29a4f5981611315f2347d62adf809ff21fd38ee
Aditya Singh <adityasingh220598@gmail.com>
2023-07-22 17:41:32 +0530

    Fix: refactoring code

--- apps/frontend/src/features/Questionnaire/Questionnaire.tsx
+++ apps/frontend/src/features/Questionnaire/Questionnaire.tsx
@@ -37,7 +37,7 @@ export type QuestionnaireProps = {
   advantages?: LandingPage['questionnaires_advantages'];
   customSelectHandler?: SingleChoiceEventHandler;
   autocomplete_states?: LandingPage['states_autocomplete'];
-  company_id: LandingPage['company_id']
+  company_id: LandingPage['company_id'];
 };
 
 export const Questionnaire: React.FC<QuestionnaireProps> = ({
@@ -117,7 +117,10 @@ export const Questionnaire: React.FC<QuestionnaireProps> = ({
               />
             )}
             {steps.isContactFormStep && (
-              <ContactDetails staticContent={staticContent} company_id={company_id} />
+              <ContactDetails
+                staticContent={staticContent}
+                company_id={company_id}
+              />
             )}
             {steps.isFormSuccessStep && (
               <Confirmation phone={phone} staticContent={staticContent} />
