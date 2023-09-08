18988b5070374744c2f3bfb7c532ff13139e2a31
Aditya Singh <adityasingh220598@gmail.com>
2023-09-08 17:34:34 +0530

    Feature: font family plugin

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -7,17 +7,25 @@ import { Layout } from '../components/Layout/Layout';
 
 const IndexPage: ContentPage = ({ content, staticContent }) => {
   const SectionManager = initSectionManager(content, staticContent);
+  const font = content.selectfont;
 
   return (
-    <Layout content={content} staticContent={staticContent}>
-      <SectionContextProvider>
-        {SectionManager.Hero}
-        {SectionManager.Video}
-        {SectionManager.Services}
-        {SectionManager.Reviews}
-        {SectionManager.Questions}
-      </SectionContextProvider>
-    </Layout>
+    <>
+      <style jsx global>{`
+        body {
+          font-family: ${font};
+        }
+      `}</style>
+      <Layout content={content} staticContent={staticContent}>
+        <SectionContextProvider>
+          {SectionManager.Hero}
+          {SectionManager.Video}
+          {SectionManager.Services}
+          {SectionManager.Reviews}
+          {SectionManager.Questions}
+        </SectionContextProvider>
+      </Layout>
+    </>
   );
 };
 
